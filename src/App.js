eaf69cbb27776a110efe24a803005c44b7c59b66
12VinodAcharya <achabinod1112@gmail.com>
2023-03-05 00:45:41 +0545

    New components and containers created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import './App.css'
 function App() {
   return (
-    <div>
-        <h3>GPT-3</h3>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <Blog />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
